97883e1a55aad473af1508a9940517ba9ab8c071
ApenasGui <guilherme.s.costa1@gmail.com>
2026-06-04 18:44:14 -0300

    feat: backend Node.js com Express e endpoints básicos

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const pool = require('./database')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.json());
 
 app.get('/health', (req, res) => {
     return res.json('OK!');
@@ -11,6 +12,15 @@ app.get('/health', (req, res) => {
 app.get('/api/livros', async (req, res) => {
     const resultado = await pool.query('SELECT * FROM livros')
     return res.json(resultado.rows);
+})
+
+app.post('/api/livros', async (req, res) => {
+    const { titulo, genero, sinopse, estado, status, preco } = req.body;
+    const resultado = await pool.query(`
+        INSERT INTO livros(titulo, genero, sinopse, estado, status, preco)
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *
+    `, [titulo, genero, sinopse, estado, status, preco])
+    return res.json(resultado.rows[0]);
 })
 
 app.listen(PORT, () => {
